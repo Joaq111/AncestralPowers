ce166c997d6156c5acd113c69de6a5434874378b
João <joaoviniciusbrittesferreira@gmail.com>
2025-09-29 23:14:44 -0300

    Trying to add the imortal power.

--- src/main/java/dev/joaq/ancestralpowers/mixin/RenderPlayerMixin.java
+++ src/main/java/dev/joaq/ancestralpowers/mixin/RenderPlayerMixin.java
@@ -9,31 +9,31 @@ import net.minecraft.client.util.math.MatrixStack;
 
 import static java.awt.Color.*;
 
-public class RenderPlayerMixin {
-
-    @Override
-    public void render(AbstractClientPlayerEntity player, float yaw, float tickDelta, MatrixStack matrices,
-                       VertexConsumerProvider vertexConsumers, int light) {
-        PlayerTraits traits = MyComponents.TRAITS.get(player);
-
-        // Valores controlados pelo servidor via componente
-//        float red   = traits.getRed();   // 0.0 a 1.0
-//        float green = traits.getGreen(); // 0.0 a 1.0
-//        float blue  = traits.getBlue();  // 0.0 a 1.0
-//        float alpha = traits.getAlpha(); // 0.0 a 1.0
-
-        RenderSystem.enableBlend();
-        RenderSystem.defaultBlendFunc();
-
-        // 🔥 Aplica cor + transparência
-        RenderSystem.setShaderColor(red, green, blue, alpha);
-
-        super.render(player, yaw, tickDelta, matrices, vertexConsumers, light);
-
-        // Reset para não afetar outros renders
-        RenderSystem.setShaderColor(1f, 1f, 1f, 1f);
-        RenderSystem.disableBlend();
-    }
-
-
-}
+//public class RenderPlayerMixin {
+//
+//    @Override
+//    public void render(AbstractClientPlayerEntity player, float yaw, float tickDelta, MatrixStack matrices,
+//                       VertexConsumerProvider vertexConsumers, int light) {
+//        PlayerTraits traits = MyComponents.TRAITS.get(player);
+//
+//        // Valores controlados pelo servidor via componente
+////        float red   = traits.getRed();   // 0.0 a 1.0
+////        float green = traits.getGreen(); // 0.0 a 1.0
+////        float blue  = traits.getBlue();  // 0.0 a 1.0
+////        float alpha = traits.getAlpha(); // 0.0 a 1.0
+//
+//        RenderSystem.enableBlend();
+//        RenderSystem.defaultBlendFunc();
+//
+//        // 🔥 Aplica cor + transparência
+//        RenderSystem.setShaderColor(red, green, blue, alpha);
+//
+//        super.render(player, yaw, tickDelta, matrices, vertexConsumers, light);
+//
+//        // Reset para não afetar outros renders
+//        RenderSystem.setShaderColor(1f, 1f, 1f, 1f);
+//        RenderSystem.disableBlend();
+//    }
+//
+//
+//}
